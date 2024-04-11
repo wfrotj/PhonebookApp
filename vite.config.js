ebe703e1da68194ae43d726331de0040fb0a062e
wfrotj <trebrodrigo@gmail.com>
2024-04-11 19:57:10 +0800

    edit vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  server: {
-    proxy: "https://phonebook-application-4baq.onrender.com",
+  build: {
+    outDir: "dist",
   },
+  /*   server: {
+    proxy: "http://localhost:3005",
+  }, */
 });
